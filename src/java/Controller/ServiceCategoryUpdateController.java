ba9c6e96731bd61548f2111171c954bfd5963a2f
AnPham <95533400+Oversoul1113@users.noreply.github.com>
2022-07-18 11:25:39 +0700

    update service category

--- src/java/Controller/ServiceCategoryUpdateController.java
+++ src/java/Controller/ServiceCategoryUpdateController.java
@@ -39,7 +39,7 @@ public class ServiceCategoryUpdateController extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ServiceCategoryUpdateController</title>");            
+            out.println("<title>Servlet ServiceCategoryUpdateController</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ServiceCategoryUpdateController at " + request.getContextPath() + "</h1>");
@@ -76,15 +76,17 @@ public class ServiceCategoryUpdateController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            ServiceCategoryDAO scd = new ServiceCategoryDAO();
-            ServiceCategory sc = new ServiceCategory();
-            
-            int id = Integer.parseInt(request.getParameter("id"));
-            String serviceCategory = request.getParameter("serviceCategory");
-         
-            sc.setCategory(serviceCategory);
-            scd.updateServiceCategory(id, sc);
-            response.sendRedirect("serviceCategoryManager");
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
+        ServiceCategoryDAO scd = new ServiceCategoryDAO();
+        ServiceCategory sc = new ServiceCategory();
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String serviceCategory = request.getParameter("serviceCategory");
+
+        sc.setCategory(serviceCategory);
+        scd.updateServiceCategory(id, sc);
+        response.sendRedirect("serviceCategoryManager");
     }
 
     /**
