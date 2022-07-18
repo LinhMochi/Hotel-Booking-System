e89548ed4654351e99603ce04461910b627f7173
AnPham <95533400+Oversoul1113@users.noreply.github.com>
2022-07-18 11:21:52 +0700

    List of gallery

--- src/java/Controller/galleryManagerController.java
+++ src/java/Controller/galleryManagerController.java
@@ -5,11 +5,16 @@
  */
 package Controller;
 
+import DAO.HotelDAO;
 import DAO.HotelGalleryDAO;
+import Model.Hotel;
 import Model.HotelGallery;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,14 +37,37 @@ public class galleryManagerController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    private static final int NUMBER_IMAGE = 10;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            String page;
+            try {
+                page = request.getParameter("page");
+                if (page == null) {
+                    page = "1";
+                }
+            } catch (Exception e) {
+                page = "1";
+            }
             HotelGalleryDAO hgd = new HotelGalleryDAO();
-            ArrayList<HotelGallery> list = hgd.getAllGallery();
+            int count = hgd.getAllGallery().size();
+            int endPage = count / NUMBER_IMAGE;
+            if (count % NUMBER_IMAGE != 0) {
+                endPage++;
+            }
+            ArrayList<HotelGallery> list = hgd.getGallery(page, NUMBER_IMAGE);
+            ArrayList<Hotel> hlist = new HotelDAO().getAllHotel();
             request.setAttribute("list", list);
+            request.setAttribute("hlist", hlist);
+            request.setAttribute("endPage", endPage);
+            request.setAttribute("page", page);
+            request.setAttribute("count", count);
+            request.setAttribute("numberOfImage", NUMBER_IMAGE);
             request.getRequestDispatcher("galleryManager.jsp").forward(request, response);
         }
     }
@@ -56,7 +84,11 @@ public class galleryManagerController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(galleryManagerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -70,7 +102,11 @@ public class galleryManagerController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(galleryManagerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
